11d5c364cc834825c008c2e4997b80bebfb1eb51
Dominic Bradfute <Domohkiin@Dominics-Air.risebroadband.com>
2017-10-10 20:57:25 -0500

    stuffs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,51 +6,79 @@ var pets =
 //the var where the guessed letters are put
 var guessedLetters = [];
 
+var correctGuesses = [];
+
 //word to guess
 var wordToGuess = null;
-
-//Number of guesses you start with
-var guessesLeft = null;
 
 //Win to loss counter
 var wins = 0;
 var losses = 0;
 
-//randmly selects a word from the pets array
-var word = pets[Math.floor(Math.random() * pets.length)];
+//Number of guesses you start with
+var guessesLeft = [];
 
 //separates the selected word and divides it into individual letters
 //var letters = word.split("");
 
 //generates underscores equal to the length of the selected word and
 //puts them on the page
-var underscore = "";
-for(i=0; i<word.length; i++) {
-	underscore = underscore + "_ "
-}
-document.querySelector("#current-word").innerHTML = underscore;
 
+function startGame() {
+//randomly selects a word from the pets array
+word = pets[Math.floor(Math.random() * pets.length)];
+
+for (var i = 0; i < word.length; i++) {
+	correctGuesses.push('_');
+
+}
+
+guessesLeft = word.length + 5;
+
+document.querySelector("#current-word").innerHTML = correctGuesses.join(' ');
+}
 //updates the total guesses var
-var updateGuessesLeft = function(){
-	guessesLeft = word.length + 5;
+function updateGuesses(letter){
+	guessesLeft --;
 	document.querySelector("#guesses-left").innerHTML = guessesLeft;
+
+	if (word.indexOf(letter) === -1) {
+		guessedLetters.push(letter);
+		document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(', ');
+	} else {
+		
+		for (var i = 0; i< word.length; i++) {
+			if (word[i] === letter) {
+				correctGuesses[i] = letter;
+			}
+		}
+		document.querySelector("#current-word").innerHTML = correctGuesses.join(' ');
+	}
+
 }
 
-updateGuessesLeft();
+function checkWin() {
+	if (correctGuesses.indexOf('_') === -1) {
+		wins++;
+		document.querySelector("#wins").innerHTML = wins;
+		startGame();
+	} else if (guessesLeft === 0) {
+		losses --;
+		document.querySelector("#losses").innerHTML = losses;
+		startGame();
+	}
+}
 
 //Detects which key is pressed
 document.onkeyup = function(event) {
 
-	//Reduces the guesses left by one everytime a key is pressed 
-	guessesLeft--;
-
 	//turns all inputed letters to lower case
-	var key = String.fromCharCode(event.keycode).toLowerCase();
+	var letterGuessed = String.fromCharCode(event.keycode).toLowerCase();
 
-	//pushes guessed letters into the guessedLetters array
-	guessedLetters.push(choice);
+	//Reduces the guesses left by one everytime a key is pressed 
+	updateGuesses(letterGuessed);
+	checkWin();
+	
+};
 
-	//Checks if the guessed letter is in the word
-	for(str.search("key")>= 0)
-		underscore = str.replace()
-}
+startGame();
